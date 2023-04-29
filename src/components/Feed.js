7a8a05fdb9f744d6af7c859054867be359b26c1b
harshal <harshalc562@gmail.com>
2023-04-30 03:22:47 +0530

    Youtube clone using MUI

---
+++ src/components/Feed.js
@@ -0,0 +1,55 @@
+import React, { useState, useEffect } from 'react'
+import { Box, Typography, Stack } from '@mui/material'
+import SideBar from './SideBar'
+import Videos from './Videos'
+import { fetchFromAPI } from '../utils/fetchFromAPI'
+
+const Feed = () => {
+
+  const [selectedCategory, setSelectedCategory] = useState('New')
+  const [videos, setVideos] = useState([])
+
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`)
+      .then((data) => setVideos(data.items));
+
+  }, [selectedCategory])
+
+  return (
+    <Stack
+      sx={{
+        flexDirection:
+        {
+          sx: 'column', md: 'row'
+        }
+      }}>
+      <Box
+        sx={{
+          height:
+            { sx: 'auto', md: '92vh' },
+          borderRight: '1px solid #3d3d3d',
+          px: { sx: 0, md: 2 }
+        }}>
+        <SideBar
+          selectedCategory={selectedCategory}
+          setSelectedCategory={setSelectedCategory}
+        />
+        <Typography
+          className='copyright'
+          variant='body2'
+          sx={{ color: '#fff', mt: 1.5 }}
+        >
+          Copyright 2023 JSM Media
+        </Typography>
+      </Box>
+
+      <Box p={2} sx={{ overflow: 'auto', height: '90vh', flex: 2 }}>
+        <Typography variant='h4' fontWeight='bold' mb={2} color='white'>
+          {selectedCategory}<span style={{ color: 'red' }}> Videos</span>
+        </Typography>
+      </Box>
+      <Videos videos={videos} />
+    </Stack>
+  )
+}
+export default Feed
---
+++ src/utils/fetchFromAPI.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+const BASE_URL = 'https://youtube-v31.p.rapidapi.com'
+
+const options = {
+  params: {
+    maxResults: '50',
+  },
+  headers: {
+    'X-RapidAPI-Key': '43ea6b997emshe8dce86410ce6b3p16f784jsneb8e8ed918f6',
+    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
+  }
+};
+
+export const fetchFromAPI = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options)
+  console.log(data)
+  return data;
+}
