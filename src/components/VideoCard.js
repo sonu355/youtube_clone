abbb07051423d52a5665783c5932fbf55c98380c
harshal <harshalc562@gmail.com>
2023-04-30 14:43:17 +0530

    VideoCard

---
+++ src/components/VideoCard.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { Typography, Card, CardContent, CardMedia } from '@mui/material'
+import { CheckCircle } from '@mui/icons-material'
+import {demoThumbnailUrl} from '../utils/Constant'
+
+const VideoCard = () => {
+  return (
+    <div>
+      VideoCard
+    </div>
+  )
+}
+
+export default VideoCard
